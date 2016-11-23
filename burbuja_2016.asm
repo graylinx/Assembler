6cdf4b562e5501ed44a6797446a5fd2c9875a6b0
Graylinx <mariofg4@hotmail.com>
2016-11-23 19:06:56 +0100

    newburbu

--- burbuja_2016.asm
+++ burbuja_2016.asm
@@ -1,42 +1,66 @@
 .data
-Array: .word 2,5,1,4,0
+	Array: .word 8,1,2,4,0
 .text
 
 main:
 	la $a0, Array
 	jal burbuja
-	move $v0, $a0
+	move $a0, $v0
 	jal print
+exit:
 	li $v0, 10
 	syscall
 
 burbuja:
+	subu $sp, $sp, 32
+	sw $fp, ($sp)
+	sw $ra, 4($sp)
+	addi $fp, $sp, 8
+	sw $a0, ($fp)
+	
+	li $t0, 0
+	li $t1, 0
+	
+loop_i:
+	
+	blt $t0, 4 loop_j
+	addi $t1, $t1, 1
+	li $t0, 0
+	blt $t1, 4 loop_i
+	lw $v0, ($fp)
+	lw $fp, ($sp)
+	lw $fp, 4($sp)
+	addi $sp, $sp, 32
+	jr $ra
+	
+loop_j:
+	
+	bge $t0, 4 loop_i
+	mul $t2, $t0, 4
+	lw $t3, Array($t2)
+	addi $t4, $t2, 4
+	lw $t5, Array($t4)
+	addi $t0, $t0, 1
+
+	blt $t3, $t5, loop_j
+	sw $t5, Array($t2)
+	sw $t3, Array($t4)
+	b loop_j
+
+
+print:
 	li $t0, 0
 loop:
-	addi $t0, $t0, 1
-	li $t1, 0
-	blt $t1, 4, loop_1
-	la $v0, Array
-	jr $ra
-loop_1:
-	
-	lw $t2, Array($t1)
-	addi $t1,$t1, 1
-	lw $t3, Array($t1)
-	bge $t2, $t3, loop_1
-	mul $t4, $t1, 4
-	addi $t5, $t4, 4
-	sw $t3, Array($t4)
-	sw $t2, Array($t5)
-	b loop_1
-	
-print:
-	la $a0, ($a0)
+	bgt $t0, 4, exit
+	subu $sp, $sp, 32
+	sw $fp, ($sp)
+	sw $ra, 4($sp)
+	addi $fp, $sp, 28
+	sw $a0, ($fp)
+	lw $a0, ($a0)
 	li $v0, 1
 	syscall
-	addi $a0, $a0, 4
 	addi $t0, $t0, 1
-	blt $t0, 5, print 
-	jr $ra
-		
-	
+	lw $a0, ($fp)
+	add $a0, $a0, 4
+	b loop
